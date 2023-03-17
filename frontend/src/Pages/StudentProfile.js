d3d2657effa9e765c35c50a1c0b2eea32407956b
Riya Garg <riya009btece21@igdtuw.ac.in>
2023-03-17 18:17:17 +0530

    a small issue fixed

--- frontend/src/Pages/StudentProfile.js
+++ frontend/src/Pages/StudentProfile.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import './Profile.css';
-import SideBar from "../Components/SideBar";
+import React from "react";
+import "./Profile.css";
+import SideBar from "../Components/SideBarStudent";
 import ProfileForm from "../Components/ProfileForm";
 function StudentProfile() {
   return (
     <div>
-     <SideBar></SideBar>
-     <ProfileForm></ProfileForm>
+      <SideBar></SideBar>
+      <ProfileForm></ProfileForm>
     </div>
-  )
+  );
 }
 
-export default StudentProfile
+export default StudentProfile;
